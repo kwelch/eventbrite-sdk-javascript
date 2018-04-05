a73f67216552f5be163dbfa6d847604bcecc86b9
David Ardoin <37851640+dtardoin-eb@users.noreply.github.com>
2018-04-05 15:46:46 -0700

    chore(build): Add encrypted ssh key for semantic-release (#20)
    To allow Travis to automatically push tags back into Github.
    
    Fixes OPS-16282 & blocks #17

---
+++ _scripts/deploy_key_decrypt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+# Decrypt Github Deploy key for the Semantic Release package to be able to push tags 
+# Ubuntu-14.04
+
+# Make sure only the current user can read the private key- 
+/usr/bin/openssl aes-256-cbc -K "${encrypted_ba12a3340263_key}" -iv "${encrypted_ba12a3340263_iv}" -in git_deploy_key.enc -out /tmp/git_deploy_key -d 
+# Make sure only the current user can read the private key
+/bin/chmod 600 /tmp/git_deploy_key
+# Create a script to return the passphrase environment variable to ssh-add
+/bin/echo 'echo ${SSH_PASSPHRASE}' > /tmp/askpass && chmod +x /tmp/askpass
+# Start the authentication agent
+eval "$(ssh-agent -s)"
+# Add the key to the authentication agent
+DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" /usr/bin/setsid ssh-add /tmp/git_deploy_key </dev/null
+
