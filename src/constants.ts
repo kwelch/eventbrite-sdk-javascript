1b1a5e95926bbd26d100151a8e42e21d89c9dfe2
Kyle Welch <kwelch0626@gmail.com>
2018-10-24 14:33:57 -0500

    fixed up exported constants and removed map

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,2 @@
-export const PAGINATION_TYPE = {
-    PAGINATION: 'page',
-    CONTINUATION: 'continuation',
-};
-
-export const PAGINATION_MAP = {
-    [PAGINATION_TYPE.PAGINATION]: {
-        hasMoreItems: ({page_number: pageNumber, page_count: pageCount}) =>
-            pageNumber >= pageCount,
-        getNextPage: ({page_number: pageNumber}) => pageNumber + 1,
-        initialPageId: 1,
-    },
-    [PAGINATION_TYPE.CONTINUATION]: {
-        donePredicate: ({has_more_items: hasMoreItems}) => !hasMoreItems,
-        getNextPage: ({continuation}) => continuation,
-        initialPageId: null,
-    },
-};
+export const PAGINATION = 'page';
+export const CONTINUATION = 'continuation';
