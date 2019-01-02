ab23e91f0b48575d685cdfb7527e64f1ac75d227
Kyle Welch <kwelch0626@gmail.com>
2019-01-02 16:40:57 -0600

    add keys to UserObject interface to fix lint issue

--- src/users.ts
+++ src/users.ts
@@ -1,13 +1,25 @@
 import {RequestHelper} from './types';
 
-export interface UserObject {}
+export interface UserObject {
+    id: string;
+    first_name: string;
+    last_name: string;
+    image_id: string;
+    email: [
+        {
+            email: string;
+            primary: true;
+        }
+    ];
+}
 
 export interface UserCollection {
     [key: string]: () => Promise<UserObject>;
 }
 
 export default (request: RequestHelper): UserCollection => {
-    const me = (): Promise<UserObject> => request('/users/me/');
+    const me = (): Promise<UserObject> =>
+        request('/users/me/') as Promise<UserObject>;
 
     return {
         me,
