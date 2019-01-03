c232d53aa3c80c59111522054d7da7c770232435
Kyle Welch <kwelch0626@gmail.com>
2019-01-03 13:46:35 -0600

    Add Email type and rename UserObject to User

--- src/users.ts
+++ src/users.ts
@@ -1,24 +1,24 @@
 import {JSONRequest} from './types';
 
-export interface UserObject {
+export type Email = {
+    email: string;
+    primary: true;
+};
+
+export interface User {
     id: string;
     first_name: string;
     last_name: string;
     image_id: string;
-    email: [
-        {
-            email: string;
-            primary: true;
-        }
-    ];
+    email: Email[];
 }
 
 export interface UserMethods {
-    me: () => Promise<UserObject>;
+    me: () => Promise<User>;
 }
 
 export default (request: JSONRequest): UserMethods => {
-    const me = () => request('/users/me/') as Promise<UserObject>;
+    const me = () => request('/users/me/') as Promise<User>;
 
     return {
         me,
